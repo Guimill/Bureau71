02fab3d7dbd79779b76e314790c6e0e9296c895a
Guimill <guillaume.milleret@gmail.com>
2023-08-15 15:19:56 +0200

    Update index.jsx

--- bureau71/src/components/navbar/index.jsx
+++ bureau71/src/components/navbar/index.jsx
@@ -2,33 +2,33 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 export default function Navbar() {
 
-  const [buttonFormationDisabled, setButtonFormationDisabled] = useState(false);
-  const [buttonExperiencesDisabled, setButtonExperiencesDisabled] = useState(false);
-  const [ButtonRealisationDisabled, setButtonRealisationDisabled] = useState(false);
-  const [ButtonCompetencesDisabled, setButtonCompetencesDisabled] = useState(false);
+  const [buttonFormationDisabled, setButtonIndexDisabled] = useState(false);
+  const [buttonExperiencesDisabled, setButtonPrestationsDisabled] = useState(false);
+  const [ButtonRealisationDisabled, setButtonContactDisabled] = useState(false);
+  const [ButtonCompetencesDisabled, setButtonBlogDisabled] = useState(false);
 
 
   return (
     <div className="navbar">
-      <Link to={'/formation'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
+      <Link to={'/index'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
         <button disabled={buttonFormationDisabled}
-          onClick={() => { setButtonFormationDisabled(true); setButtonExperiencesDisabled(false); setButtonRealisationDisabled(false); setButtonCompetencesDisabled(false) }}>
-          Formation
+          onClick={() => { setButtonIndexDisabled(true); setButtonPrestationsDisabled(false); setButtonContactDisabled(false); setButtonBlogDisabled(false) }}>
+          Index
         </button></Link>
-      <Link to={'/experiences'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
+      <Link to={'/prestations'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
         <button disabled={buttonExperiencesDisabled}
-          onClick={() => { setButtonExperiencesDisabled(true); setButtonRealisationDisabled(false); setButtonFormationDisabled(false); setButtonCompetencesDisabled(false) }}>
-          Experiences
+          onClick={() => { setButtonPrestationsDisabled(true); setButtonContactDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(false) }}>
+          Prestations
         </button></Link>
-      <Link to={'/competences'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
+      <Link to={'/blog'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
         <button disabled={ButtonCompetencesDisabled}
-          onClick={() => { setButtonRealisationDisabled(false); setButtonExperiencesDisabled(false); setButtonFormationDisabled(false); setButtonCompetencesDisabled(true) }}>
-          Competences
+          onClick={() => { setButtonContactDisabled(false); setButtonPrestationsDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(true) }}>
+          Blog
         </button></Link>
-      <Link to={'/realisations'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
+      <Link to={'/contact'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
         <button disabled={ButtonRealisationDisabled}
-          onClick={() => { setButtonRealisationDisabled(true); setButtonExperiencesDisabled(false); setButtonFormationDisabled(false); setButtonCompetencesDisabled(false) }}>
-          Realisations
+          onClick={() => { setButtonContactDisabled(true); setButtonPrestationsDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(false) }}>
+          Contact
         </button></Link>
     </div>
   );
