32aad63b5c815ecbccc3c38805a9c8c383ad6e05
Guimill <guillaume.milleret@gmail.com>
2023-08-15 17:55:16 +0200

    test

--- bureau71/src/routes/index.jsx
+++ bureau71/src/routes/index.jsx
@@ -2,12 +2,16 @@ import fleur from "../assets/img/headpic.jpg"
 
 export default function Index() {
   return (
-    <div>
-      <main className='index'>
-      <div>
-        <img style={{height:"100vh", width:"100vw"}} src={fleur} alt="frfr" />
-    </div>
-      </main>
+    <div class="wrapper">
+      <div class="div1"> <img src={fleur} alt="" /> </div>
+      <div class="div2"> <img src={fleur} alt="" /></div>
+      <div class="div3"> </div>
+      <div class="div4"> </div>
+      <div class="div5"> <img src={fleur} alt="" /></div>
+      <div class="div6"> <img src={fleur} alt="" /></div>
+      <div class="div7"> </div>
+      <div class="div8"> </div>
+      <div class="div9"> <img src={fleur} alt="" /></div>
     </div>
   );
 }
