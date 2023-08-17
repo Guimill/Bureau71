81f90918f4e78b21074944aec98cbb1dca9c463f
Guimill <86968380+Guimill@users.noreply.github.com>
2023-08-17 14:49:45 +0200

    Update index.jsx

--- bureau71/src/routes/index.jsx
+++ bureau71/src/routes/index.jsx
@@ -5,12 +5,13 @@ export default function Index() {
     <div class="wrapper">
       <div class="div1"> <img src={fleur} alt="" /> </div>
       <div class="div2"> <img src={fleur} alt="" /></div>
-      <div class="div3"> </div>
-      <div class="div4"> </div>
+      <div class="div3"> <p> Bureau71 c'est une aventure dans l'univers de l'écologie et l'urbanisme !
+      Une référence à la convention Ramsar pionnière dans la protection des zones humides</p> </div>
+      <div class="div4"> <p> En tant qu'écologue et cartographe j'interviens en soutien de vos projets via des délimitations de zones humides, de la rédactions de rapport et prochainement des études faunes. </p> </div>
       <div class="div5"> <img src={fleur} alt="" /></div>
       <div class="div6"> <img src={fleur} alt="" /></div>
-      <div class="div7"> </div>
-      <div class="div8"> </div>
+      <div class="div7"> <p> Ancien guide du prestigieux muséum d'histoire naturelle de Toulouse, je vous propose des activités naturalistes et vous propose mes talents de vulgarisateurs scientifiques ! </p> </div>
+      <div class="div8"> <p> Je suis a </p> </div>
       <div class="div9"> <img src={fleur} alt="" /></div>
     </div>
   );
