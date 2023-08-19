26ae6b96405046fe11f834e55b669977b78be6ad
Guimill <guillaume.milleret@gmail.com>
2023-08-19 17:52:17 +0200

    rfrf

--- bureau71/src/routes/index.jsx
+++ bureau71/src/routes/index.jsx
@@ -4,17 +4,15 @@ export default function Index() {
   return (
     <div class="wrapper">
     <div class="div1"></div>
-    <div class="div2"> <img src={fleur} alt="" /></div>
+    <div class="div2"> <div className="div2__background"></div> </div>
     <div class="div3"> Bureau71 c'est une aventure dans l'univers de l'écologie et l'urbanisme !
         Une référence à la convention Ramsar pionnière dans la protection des zones humides</div>
     <div class="div4"> En tant qu'écologue et cartographe j'interviens en soutien de vos projets via des délimitations de zones humides, de la rédactions de rapport et prochainement des études faunes.</div>
-    <div class="div5"> <img src={fleur} alt="" /></div>
-    <div class="div6"> <img src={fleur} alt="" /></div>
+    <div class="div5"> <div className="div5__background"></div> </div>
+    <div class="div6"> <div className="div6__background"></div> </div>
     <div class="div7"> Ancien guide du prestigieux muséum d'histoire naturelle de Toulouse, je vous propose des activités naturalistes et vous propose mes talents de vulgarisateurs scientifiques !</div>
     <div class="div8"> Je suis a</div>
     <div class="div9">vtvtv</div>
-    </div> 
-
-    
+    </div>    
   );
 }
