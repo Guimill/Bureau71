ac2d51247784b42590967202d513a157708517ef
Guimill <guillaume.milleret@gmail.com>
2023-08-23 18:45:26 +0200

    navbar

--- bureau71/src/components/navbar/index.jsx
+++ bureau71/src/components/navbar/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+
 export default function Navbar() {
 
   const [buttonFormationDisabled, setButtonIndexDisabled] = useState(false);
@@ -7,29 +8,49 @@ export default function Navbar() {
   const [ButtonRealisationDisabled, setButtonContactDisabled] = useState(false);
   const [ButtonCompetencesDisabled, setButtonBlogDisabled] = useState(false);
 
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = event => {
+    setIsShown(current => !current);
+  };
 
   return (
-    <div className="navbar">
-      <Link to={'/'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
-        <button disabled={buttonFormationDisabled}
-          onClick={() => { setButtonIndexDisabled(true); setButtonPrestationsDisabled(false); setButtonContactDisabled(false); setButtonBlogDisabled(false) }}>
-          Index
-        </button></Link>
-      <Link to={'/prestations'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
-        <button disabled={buttonExperiencesDisabled}
-          onClick={() => { setButtonPrestationsDisabled(true); setButtonContactDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(false) }}>
-          Prestations
-        </button></Link>
-      <Link to={'/blog'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
-        <button disabled={ButtonCompetencesDisabled}
-          onClick={() => { setButtonContactDisabled(false); setButtonPrestationsDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(true) }}>
-          Blog
-        </button></Link>
-      <Link to={'/contact'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
-        <button disabled={ButtonRealisationDisabled}
-          onClick={() => { setButtonContactDisabled(true); setButtonPrestationsDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(false) }}>
-          Contact
-        </button></Link>
-    </div>
+    <>
+      <div className="navbar">
+        {isShown && (
+        <ul>
+          <li>
+            <Link to={'/'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
+              <button disabled={buttonFormationDisabled}
+                onClick={() => { setButtonIndexDisabled(true); setButtonPrestationsDisabled(false); setButtonContactDisabled(false); setButtonBlogDisabled(false) }}>
+                Acceuil
+              </button></Link>
+          </li>
+          <li>
+            <Link to={'/prestations'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
+              <button disabled={buttonExperiencesDisabled}
+                onClick={() => { setButtonPrestationsDisabled(true); setButtonContactDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(false) }}>
+                Prestations
+              </button></Link>
+          </li>
+          <li>
+            <Link to={'/blog'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
+              <button disabled={ButtonCompetencesDisabled}
+                onClick={() => { setButtonContactDisabled(false); setButtonPrestationsDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(true) }}>
+                Blog
+              </button></Link>
+          </li>
+          <li>
+            <Link to={'/contact'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
+              <button disabled={ButtonRealisationDisabled}
+                onClick={() => { setButtonContactDisabled(true); setButtonPrestationsDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(false) }}>
+                Contact
+              </button></Link>
+          </li>
+        </ul>
+        )}
+        <div onClick={handleClick}></div>
+      </div>
+    </>
   );
 }
