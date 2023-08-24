84642be5be48a85be4c8243e550603e2ba3ded19
Guimill <guillaume.milleret@gmail.com>
2023-08-24 08:15:20 +0200

    navbar

--- bureau71/src/components/navbar/index.jsx
+++ bureau71/src/components/navbar/index.jsx
@@ -7,47 +7,56 @@ export default function Navbar() {
   const [buttonExperiencesDisabled, setButtonPrestationsDisabled] = useState(false);
   const [ButtonRealisationDisabled, setButtonContactDisabled] = useState(false);
   const [ButtonCompetencesDisabled, setButtonBlogDisabled] = useState(false);
-
   const [isShown, setIsShown] = useState(false);
 
-  const handleClick = event => {
+  const handleClick = e => {
     setIsShown(current => !current);
   };
 
+  function Show() {
+    if(window.scrollY >= 920) {
+      setIsShown(true)
+    }else {
+      setIsShown(false)
+    }
+  }
+
+  window.addEventListener("scroll",Show)
+
   return (
     <>
-      <div className="navbar">
+      <div className={isShown ? "navbar scrolling" : "navbar"}>
         {isShown && (
-        <ul>
-          <li>
-            <Link to={'/'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
-              <button disabled={buttonFormationDisabled}
-                onClick={() => { setButtonIndexDisabled(true); setButtonPrestationsDisabled(false); setButtonContactDisabled(false); setButtonBlogDisabled(false) }}>
-                Acceuil
-              </button></Link>
-          </li>
-          <li>
-            <Link to={'/prestations'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
-              <button disabled={buttonExperiencesDisabled}
-                onClick={() => { setButtonPrestationsDisabled(true); setButtonContactDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(false) }}>
-                Prestations
-              </button></Link>
-          </li>
-          <li>
-            <Link to={'/blog'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
-              <button disabled={ButtonCompetencesDisabled}
-                onClick={() => { setButtonContactDisabled(false); setButtonPrestationsDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(true) }}>
-                Blog
-              </button></Link>
-          </li>
-          <li>
-            <Link to={'/contact'} onClick={() => { window.scrollTo({ top: 0, left: 0 }) }}>
-              <button disabled={ButtonRealisationDisabled}
-                onClick={() => { setButtonContactDisabled(true); setButtonPrestationsDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(false) }}>
-                Contact
-              </button></Link>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <Link to={'/'}>
+                <button disabled={buttonFormationDisabled}
+                  onClick={() => { setButtonIndexDisabled(true); setButtonPrestationsDisabled(false); setButtonContactDisabled(false); setButtonBlogDisabled(false) }}>
+                  Acceuil
+                </button></Link>
+            </li>
+            <li>
+              <Link to={'/prestations'}>
+                <button disabled={buttonExperiencesDisabled}
+                  onClick={() => { setButtonPrestationsDisabled(true); setButtonContactDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(false) }}>
+                  Prestations
+                </button></Link>
+            </li>
+            <li>
+              <Link to={'/blog'}>
+                <button disabled={ButtonCompetencesDisabled}
+                  onClick={() => { setButtonContactDisabled(false); setButtonPrestationsDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(true) }}>
+                  Blog
+                </button></Link>
+            </li>
+            <li>
+              <Link to={'/contact'}>
+                <button disabled={ButtonRealisationDisabled}
+                  onClick={() => { setButtonContactDisabled(true); setButtonPrestationsDisabled(false); setButtonIndexDisabled(false); setButtonBlogDisabled(false) }}>
+                  Contact
+                </button></Link>
+            </li>
+          </ul>
         )}
         <div onClick={handleClick}></div>
       </div>
