595303ec4c84eed83345387c9c37772a26de2290
Guimill <86968380+Guimill@users.noreply.github.com>
2023-08-29 13:17:19 +0200

    Create sidebar.jsx

---
+++ bureau71/src/components/sidebar/sidebar.jsx
@@ -0,0 +1,10 @@
+            <div>
+                <ul>
+                    <li>Delimitation de zones humides</li>
+                    <li>Balade Biodiversité</li>
+                    <li>Animations et vulgarisation</li>
+                    <li>Interventions</li>
+                    <li>Conseils RSE</li>
+                    <li>Redaction PLU / renfort équipe</li>
+                </ul>
+            </div>
