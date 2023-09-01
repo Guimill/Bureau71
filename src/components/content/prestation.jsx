e0679fe35ef7f60ec65f6b5d2a7f9082498eb62f
Guimill <86968380+Guimill@users.noreply.github.com>
2023-09-01 14:19:48 +0200

    prestation

--- src/components/content/prestation.jsx
+++ src/components/content/prestation.jsx
@@ -1,14 +1,14 @@
-import Sidebar from :"../content/sidebar"
+import Sidebar from '../content/sidebar';
 
 export default function Prestations() {
-    return (
-      <div>
-        <main className='prestations'>
-            <p>
-                faire un sidebar pour les différents type de prestation ? 
-            </p>
-        </Sidebar>
-        </main>
-      </div>
-    );
-  }
+  return (
+    <>
+      <main className="prestations">
+        <p>
+          faire un sidebar pour les différents type de prestation ?
+        </p>
+        <Sidebar />
+      </main>
+    </>
+  );
+}
