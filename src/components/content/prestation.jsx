7a496210b93a0e08c3df037092e0856a1d7b854d
Guimill <86968380+Guimill@users.noreply.github.com>
2023-12-15 15:40:21 +0000

    higuih # Please enter the commit message for your changes. Lines starting

--- src/components/content/prestation.jsx
+++ src/components/content/prestation.jsx
@@ -5,10 +5,27 @@ export default function Prestations() {
   return (
     <>
       <main className="prestations">
-        <p>
-          faire un sidebar pour les différents type de prestation ?
-        </p>
         <Sidebar />
+        <div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
       </main>
     </>
   );
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -3,12 +3,12 @@ export default function Banner() {
     return (
                 <div ClassName="sidebar">
                 <ul>
-                    <li>Delimitation de zones humides</li>
-                    <li>Balade Biodiversité</li>
-                    <li>Animations et vulgarisation</li>
-                    <li>Interventions</li>
-                    <li>Conseils RSE</li>
-                    <li>Redaction PLU / renfort équipe</li>
+                    <li>Delimitation de zones humides <img></img> </li>
+                    <li>Balade Biodiversité <img></img></li>
+                    <li>Animations et vulgarisation <img></img></li>
+                    <li>Interventions <img></img></li>
+                    <li>Conseils RSE <img></img></li>
+                    <li>Redaction PLU / renfort équipe <img></img></li>
                 </ul>
             </div>
     )
